e08a668d43410c9a436a285317a5a25514297817
Shamsul Alam Imon <imon52145@gmail.com>
2025-12-10 19:46:45 +0600

    Add data path

--- main.py
+++ main.py
@@ -10,3 +10,11 @@ from langchain_community.vectorstores import Chroma
 from langchain_community.chat_models import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+
+# Path
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+TEXT_PATH = os.path.join(BASE_DIR, "speech.txt")
+CHROMA_DIR = os.path.join(BASE_DIR, "chroma_db")
+
+EMBEDDING_MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
+OLLAMA_MODEL_NAME = "mistral"
